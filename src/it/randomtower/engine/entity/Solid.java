7564453cffdf8d63b3c347a7aa596e5805aecfc3
Gornova <a_pakla@yahoo.it>
2010-12-10 23:30:20 +0100

    Added utility constructors

--- src/it/randomtower/engine/entity/Solid.java
+++ src/it/randomtower/engine/entity/Solid.java
@@ -5,18 +5,23 @@ import org.newdawn.slick.SlickException;
 
 public class Solid extends Entity {
 
-	public Solid(float x, float y, int width, int height) {
-		super(x, y);
-		addType(SOLID);
-		setHitBox(0, 0, width, height);
+	public Solid(float x, float y, int width, int height) throws SlickException {
+		this(x, y, width, height, -1, null);
 	}
 
-	public Solid(float x, float y, int width, int height, String refImage) throws SlickException {
+	public Solid(float x, float y, int width, int height, int depth)
+			throws SlickException {
+		this(x, y, width, height, depth, null);
+	}
+
+	public Solid(float x, float y, int width, int height, int depth,
+			String refImage) throws SlickException {
 		super(x, y);
 		addType(SOLID);
 		setHitBox(0, 0, width, height);
-		currentImage = new Image(refImage);
+		this.depth = depth;
+		if (refImage != null)
+			currentImage = new Image(refImage);
 	}
-	
-	
+
 }

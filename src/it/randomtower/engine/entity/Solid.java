ce061e53a0a0725c1364d2893cbaae778debcc60
Gornova <a_pakla@yahoo.it>
2010-12-10 22:36:19 +0100

    Add new constructor for solid with reference to an image for solid

--- src/it/randomtower/engine/entity/Solid.java
+++ src/it/randomtower/engine/entity/Solid.java
@@ -1,5 +1,8 @@
 package it.randomtower.engine.entity;
 
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+
 public class Solid extends Entity {
 
 	public Solid(float x, float y, int width, int height) {
@@ -8,4 +11,12 @@ public class Solid extends Entity {
 		setHitBox(0, 0, width, height);
 	}
 
+	public Solid(float x, float y, int width, int height, String refImage) throws SlickException {
+		super(x, y);
+		addType(SOLID);
+		setHitBox(0, 0, width, height);
+		currentImage = new Image(refImage);
+	}
+	
+	
 }

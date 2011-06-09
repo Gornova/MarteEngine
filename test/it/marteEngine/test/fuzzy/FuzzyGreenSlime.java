85ef34f06fa8b2e15247601fa4d37c49d296c34d
Gornova <a_pakla@yahoo.it>
2011-06-09 22:44:57 +0200

    Fuzzy: added Slime enemy and loaded it from Tiled map, added Slime ai (not fall from platform, please) thanks to Tommy and BackToThePast for this! (see http://www.rightanglegames.com/backtothepast/index.html)

--- test/it/marteEngine/test/fuzzy/FuzzyGreenSlime.java
+++ test/it/marteEngine/test/fuzzy/FuzzyGreenSlime.java
@@ -1,6 +1,7 @@
 package it.marteEngine.test.fuzzy;
 
 import it.marteEngine.ResourceManager;
+import it.marteEngine.entity.Entity;
 import it.marteEngine.entity.PhysicsEntity;
 
 import org.newdawn.slick.GameContainer;
@@ -11,107 +12,62 @@ public class FuzzyGreenSlime extends PhysicsEntity {
 	public static final String SLIME = "slime";
 	// private Sound jumpSnd;
 
-	private boolean onGround = false;
-	private float moveSpeed = 0.5f;
-	private int jumpSpeed = 6;
+	private float moveSpeed = 1;
 
-	private boolean right = true;
+	protected boolean faceRight = false;
 
-	public FuzzyGreenSlime(float x, float y, String ref) throws SlickException {
+	public FuzzyGreenSlime(float x, float y) throws SlickException {
 		super(x, y);
 		addAnimation(ResourceManager.getSpriteSheet("slime"), "move", true, 0,
 				0, 1, 2, 3);
-
 		addType(SLIME);
-		setHitBox(0, 0, 40, 22);
-
-		// jumpSnd = ResourceManager.getSound("jump");
+		setHitBox(0, 0, 40, 20);
+		// make Slime sloow
+		maxSpeed.x = 1;
 	}
 
 	@Override
 	public void update(GameContainer container, int delta)
 			throws SlickException {
 		super.update(container, delta);
-
-		// are we on the ground?
-		onGround = false;
-		if (collide(SOLID, x+width, y + 1) != null) {
-			onGround = true;
-		} 
-
-		// set acceleration to nothing
-		acceleration.x = 0;
-
-		if (collide(SOLID, x + 1, y) == null) {
-			right = right ? false : true;
-		} else if (collide(SOLID, x - 1, y) == null) {
-			right = right ? false : true;
-		}
-
-		if (!onGround){
-			right = false;
-		}
 		
-		if (right) {
-			if (speed.x < maxSpeed.x) {
-				acceleration.x = moveSpeed;
-			}
-		} else {
-			if (speed.x > -maxSpeed.x) {
-				acceleration.x = -moveSpeed;
-			}
-		}
+		checkGround(true, false);
 		
+		if (speed.x > 0)
+			this.faceRight = true;
+		else
+			this.faceRight = false;		
+	}
 
-		// increase acceeration, if we're not going too fast
-		// if (check(CMD_LEFT) && speed.x > -maxSpeed.x) {
-		// acceleration.x = - moveSpeed;
-		// }
-		// if (check(CMD_RIGHT) && speed.x < maxSpeed.x) {
-		// acceleration.x = moveSpeed;
-		// }
+	/**
+	 * Check if falling
+	 * @param revertHorizontal
+	 * @param revertVertical
+	 */
+	public void checkGround(boolean revertHorizontal, boolean revertVertical) {
+		boolean blocked = ((FuzzyGameWorld) world).blocked(this.x
+				+ this.speed.x + ((faceRight) ? this.width : 0), this.y
+				+ this.height + 1);
+		if (!blocked) {
+//			x = previousx;
+//			y = previousy;
+			if (revertHorizontal && speed.x != 0)
+				speed.x = -speed.x;
+			if (revertVertical && speed.y != 0)
+				speed.y = -speed.y;
+		}
+	}
 
-		// friction (apply if we're not moving, or if our speed.x is larger than
-		// maxspeed)
-		// if ( (! check(CMD_LEFT) && ! check(CMD_RIGHT)) || Math.abs(speed.x) >
-		// maxSpeed.x ) {
-		// friction(true, false);
-		// }
-
-		// jump
-		// if ( pressed(CMD_JUMP) )
-		// {
-		// //normal jump
-		// if (onGround) {
-		// speed.y = -jumpSpeed;
-		// }
-		// }
-
-		// set the gravity
-		gravity(delta);
-
-		// make sure we're not going too fast vertically
-		// the reason we don't stop the player from moving too fast left/right
-		// is because
-		// that would (partially) destroy the walljumping. Instead, we just make
-		// sure the player,
-		// using the arrow keys, can't go faster than the max speed, and if we
-		// are going faster
-		// than the max speed, descrease it with friction slowly.
-		maxspeed(false, true);
-
-		// variable jumping (tripple gravity)
-		// if (speed.y < 0 && !check(CMD_JUMP)) {
-		// gravity(delta);
-		// gravity(delta);
-		// }
-
-		// set the motion. We set this later so it stops all movement if we
-		// should be stopped
-		motion(true, true);
-
-		previousx = x;
-		previousy = y;
+	@Override
+	public void collisionResponse(Entity other) {
+		// try to move in old direction
+		if (faceRight && this.speed.x == 0) {
+			this.speed.x = -moveSpeed;
+			faceRight = false;
+		} else if (!faceRight && this.speed.x == 0) {
+			this.speed.x = moveSpeed;
+			faceRight = true;
+		}
 	}
 
 }

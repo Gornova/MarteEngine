dae6fb6a4d94e5daa4854766f1e63e2553ad89ec
Gornova <a_pakla@yahoo.it>
2011-06-16 21:49:28 +0200

    added more nice effect on collect

--- test/it/marteEngine/test/fuzzy/Star.java
+++ test/it/marteEngine/test/fuzzy/Star.java
@@ -3,6 +3,9 @@ package it.marteEngine.test.fuzzy;
 import it.marteEngine.ME;
 import it.marteEngine.ResourceManager;
 import it.marteEngine.entity.Entity;
+import it.marteEngine.tween.Ease;
+import it.marteEngine.tween.NumTween;
+import it.marteEngine.tween.Tween.TweenerMode;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
@@ -13,48 +16,52 @@ public class Star extends Entity {
 
 	public static final String STAR = "star";
 	private Sound pickupstar;
-	private RingEmitter emitter;
-	private boolean emit = false;
+
+	private NumTween fadeTween = new NumTween(1, 0, 60, TweenerMode.ONESHOT,
+			Ease.QUAD_OUT, false);
+	private NumTween moveUpTween = new NumTween(0, 20, 30, TweenerMode.ONESHOT,
+			Ease.QUAD_IN, false);
+
+	private boolean toRemove = false;
 
 	public Star(float x, float y) {
 		super(x, y, ResourceManager.getImage("star"));
 		setHitBox(0, 0, currentImage.getWidth(), currentImage.getHeight());
-		
+
 		addType(STAR);
-		
+
 		pickupstar = ResourceManager.getSound("pickupstar");
-		emitter = new RingEmitter((int)x+width/2, (int)y+height/2);
 	}
-	
+
 	@Override
 	public void update(GameContainer container, int delta)
 			throws SlickException {
 		super.update(container, delta);
-		
-		if (!emit && collide(PLAYER,x,y)!=null){
-			//ME.world.remove(this);
-			if (!pickupstar.playing()){
+
+		if (!toRemove && collide(PLAYER, x, y) != null) {
+			if (!pickupstar.playing()) {
 				pickupstar.play();
 			}
-			emit = true;
-			ME.ps.addEmitter(emitter);
-			setAlarm("stopEmit", 60, false, true);
+			toRemove = true;
+		}
+
+		if (toRemove) {
+			fadeTween.update(delta);
+			moveUpTween.update(delta);
+		}
+		if (getAlpha() == 0f) {
+			ME.world.remove(this);
 		}
 	}
-	
+
 	@Override
 	public void render(GameContainer container, Graphics g)
 			throws SlickException {
-		if (!emit){
-			super.render(container, g);
-		} 
-	}
-	
-	@Override
-	public void alarmTriggered(String name) {
-		if (name.equalsIgnoreCase("stopEmit")){
-			ME.ps.removeEmitter(emitter);
-			ME.world.remove(this);
+		super.render(container, g);
+		if (toRemove) {
+			// if to remove, apply effects
+			setAlpha(fadeTween.getValue());
+			y -= moveUpTween.getValue();
 		}
 	}
 

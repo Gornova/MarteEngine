a7aeb4c1968c16083b3b68c4f7d63dd5ed233910
alb <a_pakla@yahoo.it>
2011-09-04 16:22:14 +0200

    fix background

--- test/it/marteEngine/test/fuzzy/FuzzySelectLevellWorld.java
+++ test/it/marteEngine/test/fuzzy/FuzzySelectLevellWorld.java
@@ -1,8 +1,10 @@
 package it.marteEngine.test.fuzzy;
 
 import it.marteEngine.ME;
+import it.marteEngine.ResourceManager;
 import it.marteEngine.World;
 import it.marteEngine.entity.TextEntity;
+import it.marteEngine.game.starcleaner.Background;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
@@ -18,8 +20,6 @@ public class FuzzySelectLevellWorld extends World {
 
 	public int completition = 0;
 
-	private int deads;
-
 	public FuzzySelectLevellWorld(int id) {
 		super(id);
 	}
@@ -28,7 +28,7 @@ public class FuzzySelectLevellWorld extends World {
 	public void init(GameContainer container, StateBasedGame game)
 			throws SlickException {
 		super.init(container, game);
-		// add(new Background(0, 0, ResourceManager.getImage("menu")));
+		add(new Background(0, 0, ResourceManager.getImage("menu")));
 		add(new TextEntity(160, 20, FuzzyMain.font, "Select Level"));
 	}
 
@@ -109,9 +109,9 @@ public class FuzzySelectLevellWorld extends World {
 		if (result[0] != -1) {
 			completition = (result[0] * 100) / 12;
 		}
-		if (result[1] != -1) {
-			deads = result[1];
-		}
+		// if (result[1] != -1) {
+		// deads = result[1];
+		// }
 	}
 
 	private void drawScaled(Graphics g, float scale, String text, float x,
